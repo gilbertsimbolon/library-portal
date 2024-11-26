4f868caf244268869a702f7fbd9c402fd54e3494
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-26 13:11:34 +0800

    dashboard view configuration

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('visi-misi')" :active="request()->routeIs('visi-misi')">
+                        {{ __('Visi Misi') }}
+                    </x-nav-link>
                 </div>
             </div>
 
