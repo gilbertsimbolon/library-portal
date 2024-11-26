4839a5c4022efa64e1a6d8fc15c6cc7444be177d
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-26 12:44:23 +0800

    create seeder roles and permissions

--- config/permission.php
+++ config/permission.php
@@ -4,6 +4,10 @@ return [
 
     'models' => [
 
+        'models' => [
+            'user' => App\Models\User::class,
+        ],
+
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your permissions. Of course, it
