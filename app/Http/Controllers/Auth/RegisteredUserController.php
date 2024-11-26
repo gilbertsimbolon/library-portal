3651e692a7f0e67e8a76338a2838de552b922554
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-26 14:13:18 +0800

    fix registration feature

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,8 +43,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        // return redirect(route('login', absolute: false));
+        return redirect()->route('login')->with('success', 'Registrasi Berhasil! Silahkan Log In.');
     }
 }
