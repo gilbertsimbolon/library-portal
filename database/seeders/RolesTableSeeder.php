fe5ce05a3485c88b8488f00329fdc964a187884b
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-27 15:51:17 +0800

    configure role and permission

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -13,8 +13,19 @@ class RolesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create([
-            'name' => 'admin'
-        ]);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        $roleAdmin = Role::findByName('admin');
+        $roleAdmin->givePermissionTo('roles.index');
+        $roleAdmin->givePermissionTo('roles.create');
+        $roleAdmin->givePermissionTo('roles.edit');
+        $roleAdmin->givePermissionTo('roles.delete');
+
+        $roleUser = Role::findByName('user');
+        $roleUser->givePermissionTo('users.index');
+        $roleUser->givePermissionTo('users.create');
+        $roleUser->givePermissionTo('users.edit');
+        $roleUser->givePermissionTo('users.delete');
     }
 }
