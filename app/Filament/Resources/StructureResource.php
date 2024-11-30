2d067c2f22079bbbd82a8a873e05c0b404f3ed04
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-30 23:39:55 +0800

    added additional features and structure views in the filament

---
+++ app/Filament/Resources/StructureResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StructureResource\Pages;
+use App\Filament\Resources\StructureResource\RelationManagers;
+use App\Models\Structure;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Image;
+
+class StructureResource extends Resource
+{
+    protected static ?string $model = Structure::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            FileUpload::make('structure_img')
+                ->label('Gambar Struktur')
+                ->image()
+                ->nullable()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('structure_img'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStructures::route('/'),
+            'create' => Pages\CreateStructure::route('/create'),
+            'edit' => Pages\EditStructure::route('/{record}/edit'),
+        ];
+    }
+}
