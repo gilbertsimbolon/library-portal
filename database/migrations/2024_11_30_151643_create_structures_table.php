f5e498b8ec9725145c05b0c3e6049db0220f20f3
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-30 23:22:15 +0800

    added code to migrations and models

--- database/migrations/2024_11_30_151643_create_structures_table.php
+++ database/migrations/2024_11_30_151643_create_structures_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('structures', function (Blueprint $table) {
             $table->id();
+            $table->string('structure_img');
             $table->timestamps();
         });
     }
